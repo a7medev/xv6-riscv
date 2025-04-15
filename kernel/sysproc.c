3db2a82f09661021cc529cd08a73f56b9677f480
Ahmed Mahmoud <a7med.mahmoud2004@gmail.com>
2025-04-16 01:21:18 +0200

    feat: add settickets syscall

--- kernel/sysproc.c
+++ kernel/sysproc.c
@@ -16,6 +16,21 @@ sys_exit(void)
 }
 
 uint64
+sys_settickets(void)
+{
+  int tickets;
+  argint(0, &tickets);
+  if (tickets < 1)
+    return -1;
+
+  struct proc *p = myproc();
+  acquire(&p->lock);
+  p->tickets = settickets(p->tickets, tickets);
+  release(&p->lock);
+  return 0;
+}
+
+uint64
 sys_getpid(void)
 {
   return myproc()->pid;
