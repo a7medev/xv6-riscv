6fe4cf7b01659a1659610b148adcdfde215eafed
Ahmed Mahmoud <a7med.mahmoud2004@gmail.com>
2025-04-17 23:40:43 +0200

    fix: race conditions in rwlock

--- Makefile
+++ Makefile
@@ -143,6 +143,7 @@ UPROGS=\
 	$U/_wc\
 	$U/_zombie\
 	$U/_lotterytest\
+	$U/_flocktest\
 
 fs.img: mkfs/mkfs README $(UPROGS)
 	mkfs/mkfs fs.img README $(UPROGS)
--- kernel/rwlock.h
+++ kernel/rwlock.h
@@ -1,6 +1,6 @@
 struct rwlock {
-    struct sleeplock rwlk; // the actual sleep lock acquired by writer and first reader
     struct spinlock lk;    // protects everything below
+    int locked;            // whether the lock is locked
     int readers;           // number of readers
     int writer;            // process ID of writer
 };
